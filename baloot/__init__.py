4ae9376481db1805f638c89f47cdea7cfeed7c03
tahashieenavaz <tahashieenavaz@gmail.com>
2025-12-01 00:38:19 +0100

    renames full name function

--- baloot/__init__.py
+++ baloot/__init__.py
@@ -1,10 +1,8 @@
 from .torch import acceleration_device
 from .torch import parameter_count
-
 from .files import render_template
 from .files import funnel
-
 from .seed import seed_python
 from .seed import seed_torch
 from .seed import seed_numpy
-from .seed import full_seed
+from .seed import seed_everything
--- baloot/seed.py
+++ baloot/seed.py
@@ -1,6 +1,9 @@
-def seed_torch(seed: int):
-    import torch
+import random
+import torch
+import numpy
 
+
+def seed_torch(seed: int):
     torch.manual_seed(seed)
     torch.cuda.manual_seed_all(seed)
     torch.backends.cudnn.deterministic = True
@@ -8,18 +11,31 @@ def seed_torch(seed: int):
 
 
 def seed_numpy(seed: int):
-    import numpy
-
     numpy.random.seed(seed)
 
 
 def seed_python(seed: int):
-    import random
-
     random.seed(seed)
 
 
-def full_seed(seed: int):
+def seed_gymnasium(seed: int, environment: object):
+    environment.reset(seed=seed)
+
+    try:
+        environment.action_space.seed(seed)
+    except Exception:
+        pass
+
+    try:
+        environment.observation_space.seed(seed)
+    except Exception:
+        pass
+
+
+def seed_everything(seed: int, gym_environment: object | None = None):
     seed_python(seed)
     seed_numpy(seed)
     seed_torch(seed)
+
+    if gym_environment is not None:
+        seed_gymnasium(seed, gym_environment)
