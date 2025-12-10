2d3ff0424f1037938758e0a356cba0b10898ca8c
tahashieenavaz <tahashieenavaz@gmail.com>
2025-12-10 23:00:18 +0100

    adds sanity check

--- baloot/chart.py
+++ baloot/chart.py
@@ -5,5 +5,9 @@ def plot(x, y=None):
     if y is None:
         y = x
         x = list(range(len(y)))
+
+    if len(x) != len(y):
+        raise ValueError(f"Length mismatch: x has {len(x)} elements, y has {len(y)}.")
+
     plt.plot(x, y)
     plt.show()
