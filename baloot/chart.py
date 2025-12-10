ce733bcdfc05d07f1ea79b2f6b9112ca7783f2a0
tahashieenavaz <tahashieenavaz@gmail.com>
2025-12-10 23:02:15 +0100

    adds plot input data type

--- baloot/chart.py
+++ baloot/chart.py
@@ -1,9 +1,11 @@
 import matplotlib.pyplot as plt
 import numpy
-from typing import Union, List
+from typing import Union, List, Optional
+
+PlotInputDataType = Union[List[float], numpy.ndarray]
 
 
-def plot(x, y=None):
+def plot(x: PlotInputDataType, y: Optional[PlotInputDataType] = None):
     if y is None:
         y = x
         x = list(range(len(y)))
