c5007e3fc7669cd550a11f3b6ff4cc8be9a4a7f0
tahashieenavaz <tahashieenavaz@gmail.com>
2025-12-13 01:06:38 +0100

    updates package init file

--- baloot/__init__.py
+++ baloot/__init__.py
@@ -1,13 +1,19 @@
 from .torch import acceleration_device
 from .torch import parameter_count
+from .torch import ksg_mi
+from .torch import randomly_replace_layers
+
 from .files import render_template
 from .files import funnel
+
 from .seed import seed_python
 from .seed import seed_torch
 from .seed import seed_numpy
 from .seed import seed_everything
 from .seed import seed_gym, seed_gymnasium
+
 from .settings import settings
 from .settings import get_settings
 from .settings import reload_settings
+
 from .chart import plot
