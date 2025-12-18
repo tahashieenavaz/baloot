3dacff9a6273d91e864c09340bc3670b632b866a
tahashieenavaz <tahashieenavaz@gmail.com>
2025-12-18 21:33:00 +0100

    adds patch embedding module

--- baloot/modules/torch.py
+++ baloot/modules/torch.py
@@ -6,3 +6,31 @@ class Reshape(torch.nn.Module):
         B = x.shape[0]
         DIM = x.shape[1]
         return x.view(B, -1, DIM)
+
+
+class PatchEmbedding(torch.nn.Module):
+    def __init__(
+        self,
+        channels: int,
+        features: int,
+        patch_width: int,
+        patch_height: int,
+        activate: bool = False,
+        activation=torch.nn.functional.gelu,
+    ):
+        super().__init__()
+        self.activate = activate
+        self.activation = activation
+
+        self.stream = torch.nn.Sequential(
+            torch.nn.Conv2d(channels, features, patch_width, patch_height),
+            torch.nn.GroupNorm(features // 16, features),
+        )
+
+    def forward(self, x: torch.Tensor):
+        features = self.stream(x)
+
+        if self.activate:
+            return self.activation(features)
+
+        return features
