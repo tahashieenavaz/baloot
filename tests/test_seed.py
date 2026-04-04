604481bf40df81af3f801647c3447b8f27591c93
tahashieenavaz <tahashieenavaz@gmail.com>
2026-04-04 17:19:15 +0200

    tests cleanup

--- tests/test_seed.py
+++ tests/test_seed.py
@@ -44,55 +44,5 @@ def test_seed_torch_sets_flags():
     assert torch.backends.cudnn.benchmark is False
 
 
-class DummySpace:
-    def __init__(self):
-        self.seed_value = None
-
-    def seed(self, seed):
-        self.seed_value = seed
-
-
-class DummyEnv:
-    def __init__(self, has_reset=True):
-        self.reset_called_with = None
-
-        if has_reset:
-            self.reset = self._reset
-
-        self.action_space = DummySpace()
-        self.observation_space = DummySpace()
-
-    def _reset(self, seed=None):
-        self.reset_called_with = seed
-
-
-def test_seed_gymnasium_calls_everything():
-    env = DummyEnv()
-
-    baloot.seed_gymnasium(42, env)
-
-    assert env.reset_called_with == 42
-    assert env.action_space.seed_value == 42
-    assert env.observation_space.seed_value == 42
-
-
-def test_seed_gym_observation_optional():
-    env = DummyEnv()
-
-    baloot.seed_gym(42, env, seed_observation=False)
-
-    assert env.action_space.seed_value == 42
-    assert env.observation_space.seed_value is None
-
-
-def test_seed_gym_observation_enabled():
-    env = DummyEnv()
-
-    baloot.seed_gym(42, env, seed_observation=True)
-
-    assert env.action_space.seed_value == 42
-    assert env.observation_space.seed_value == 42
-
-
 def test_seed_everything_runs_without_error():
     baloot.seed_everything(999)
