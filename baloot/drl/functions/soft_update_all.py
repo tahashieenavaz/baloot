c78ae8013704975aff9775714b27560239fcba09
tahashieenavaz <tahashieenavaz@gmail.com>
2026-05-16 16:39:31 +0200

    fixes soft update all

--- baloot/drl/functions/soft_update_all.py
+++ baloot/drl/functions/soft_update_all.py
@@ -1,13 +1,10 @@
 import torch
 from typing import List
-from itertools import chain
+from .soft_update import soft_update
 
 
 def soft_update_all(
     *, sources: List[torch.nn.Module], targets: List[torch.nn.Module], tau: float
-):
-    for source_parameter, target_parameter in chain.from_iterable(
-        *(source.parameters() for source in sources),
-        *(target.parameters() for target in targets),
-    ):
-        target_parameter.data.mul_(1 - tau).add_(source_parameter.data, alpha=tau)
+) -> None:
+    for source, target in zip(sources, targets):
+        soft_update(source=source, target=target, tau=tau)
