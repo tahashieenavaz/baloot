f1e68a23d66af217ff742168a86a01a5a3c3ac4b
tahashieenavaz <tahashieenavaz@gmail.com>
2026-06-02 15:19:03 +0200

    fixes coth

--- baloot/triangular/coth.py
+++ baloot/triangular/coth.py
@@ -1,5 +1,9 @@
-import torch
+from baloot.helpers import load_torch
 
 
-def coth(x: torch.Tensor) -> torch.Tensor:
+def coth(x):
+    torch = load_torch()
+    assert isinstance(
+        x, torch.Tensor
+    ), f"baloot.coth accepts torch.Tensor {type(x)} was given."
     return 1.0 / torch.tanh(x)
