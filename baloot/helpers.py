f07fdcb76e4a912e4e650b9b3e3431f49229987d
tahashieenavaz <tahashieenavaz@gmail.com>
2026-06-02 15:23:57 +0200

    adds numpy and torch cache

--- baloot/helpers.py
+++ baloot/helpers.py
@@ -1,18 +1,39 @@
+_torch_cache = None
+_numpy_cache = None
+
+
 def load_torch():
+    global _torch_cache
+    if _torch_cache is not None:
+        return _torch_cache
+
     try:
         import torch
     except ImportError as e:
         raise ImportError(
             "PyTorch is required but not installed. Install it with `pip install torch`."
         ) from e
+    _torch_cache = torch
     return torch
 
 
 def load_numpy():
+    global _numpy_cache
+    if _numpy_cache is not None:
+        _numpy_cache = numpy
+
     try:
         import numpy
     except ImportError as e:
         raise ImportError(
             "Numpy is required but not installed. Install it with `pip install numpy`."
         ) from e
+
+    _numpy_cache = numpy
     return numpy
+
+
+def load_pickle():
+    import pickle
+
+    return pickle
