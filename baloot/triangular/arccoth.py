8519d10920b5abde31820a4e5643d10443e2eede
tahashieenavaz <tahashieenavaz@gmail.com>
2026-06-02 15:17:07 +0200

    fixes araccoth

--- baloot/triangular/arccoth.py
+++ baloot/triangular/arccoth.py
@@ -1,5 +1,9 @@
-import torch
+from baloot.helpers import load_torch
 
 
-def arccoth(x: torch.Tensor) -> torch.Tensor:
+def arccoth(x):
+    torch = load_torch()
+    assert isinstance(
+        x, torch.Tensor
+    ), f"baloot.arccoth accepts a torch.Tensor {type(x)} was given."
     return 0.5 * torch.log((x + 1.0) / (x - 1.0))
