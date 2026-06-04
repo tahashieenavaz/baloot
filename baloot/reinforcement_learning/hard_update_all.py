2ff45dfc2af2a9bc9518a82fd29b75a025e7027c
tahashieenavaz <tahashieenavaz@gmail.com>
2026-06-04 11:02:11 +0200

    adds torch typing torch tensor

--- baloot/reinforcement_learning/hard_update_all.py
+++ baloot/reinforcement_learning/hard_update_all.py
@@ -1,7 +1,8 @@
 from typing import List
+from baloot.typing import TorchTensor
 from .hard_update import hard_update
 
 
-def hard_update_all(*, sources: List, targets: List):
+def hard_update_all(*, sources: List[TorchTensor], targets: List[TorchTensor]) -> None:
     for source, target in zip(sources, targets):
         hard_update(source=source, target=target)
