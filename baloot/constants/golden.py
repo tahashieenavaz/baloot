d5c9e92ac355290f9e426580f513b646066d8392
tahashieenavaz <tahashieenavaz@gmail.com>
2026-06-04 11:36:22 +0200

    adds golden ration

---
+++ baloot/constants/golden.py
@@ -0,0 +1,7 @@
+from baloot.helpers import load_torch
+from baloot.typing import TorchTensor
+
+
+def golden() -> TorchTensor:
+    torch = load_torch()
+    return (1.0 + torch.sqrt(torch.tensor(5.0))) / 2.0
