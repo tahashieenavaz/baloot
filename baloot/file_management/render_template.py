65bcf2b696cffe1ef09e051c4271947cf8df349b
tahashieenavaz <tahashieenavaz@gmail.com>
2026-06-04 12:41:28 +0200

    simplifies render template arguments

--- baloot/file_management/render_template.py
+++ baloot/file_management/render_template.py
@@ -1,17 +1,15 @@
 from typing import Any
 
 
-def render_template(
-    template_location: str, target_location: str, **replacements: Any
-) -> bool:
+def render_template(*, template: str, target: str, **replacements: Any) -> bool:
     try:
-        with open(template_location, "r", encoding="utf-8") as f:
+        with open(template, "r", encoding="utf-8") as f:
             content = f.read()
 
         for key, value in replacements.items():
             content = content.replace(f"%{key}%", str(value))
 
-        with open(target_location, "w", encoding="utf-8") as f:
+        with open(target, "w", encoding="utf-8") as f:
             f.write(content)
 
         return True
