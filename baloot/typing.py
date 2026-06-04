73dd43eeee15d241ca43723e179004b7a7eb345c
tahashieenavaz <tahashieenavaz@gmail.com>
2026-06-04 11:09:22 +0200

    fixes typing conditions

--- baloot/typing.py
+++ baloot/typing.py
@@ -1,4 +1,14 @@
-from typing import Any
+from typing import Any, TypeAlias, TYPE_CHECKING
 
-TorchTensor = Any
-TorchModule = Any
+if TYPE_CHECKING:
+    import torch
+
+TorchTensor: TypeAlias = Any
+TorchModule: TypeAlias = Any
+
+if TYPE_CHECKING:
+    from torch import Tensor as _Tensor
+    from torch.nn import Module as _Module
+
+    TorchTensor = _Tensor
+    TorchModule = _Module
