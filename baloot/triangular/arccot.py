fcd58ef350f2675a54629418a8764d309d0fcb6c
tahashieenavaz <tahashieenavaz@gmail.com>
2026-06-05 16:30:18 +0200

    makes arccot to get int and torch tensor

--- baloot/triangular/arccot.py
+++ baloot/triangular/arccot.py
@@ -2,9 +2,10 @@ from baloot.helpers import load_torch
 from baloot.typing import TorchTensor
 
 
-def arccot(x: TorchTensor) -> TorchTensor:
+def arccot(x: TorchTensor | int) -> TorchTensor:
     torch = load_torch()
-    assert isinstance(
-        x, torch.Tensor
-    ), f"baloot.arccot accepts torch.Tensor {type(x)} was given."
+
+    if isinstance(x, int):
+        x = torch.tensor(x)
+
     return (torch.pi / 2) - torch.arctan(x)
