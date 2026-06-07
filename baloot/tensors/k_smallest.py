fc5e9cc38d0ac47e3559a20f556824fdcfe9e03a
tahashieenavaz <tahashieenavaz@gmail.com>
2026-06-07 18:01:37 +0200

    adds type hint to k smallest function

--- baloot/tensors/k_smallest.py
+++ baloot/tensors/k_smallest.py
@@ -1,5 +1,7 @@
 from baloot.typing import TorchTensor
 
 
-def k_smallest(x: TorchTensor, k: int, dim: int = -1, sorted: bool = True):
+def k_smallest(
+    x: TorchTensor, k: int, dim: int = -1, sorted: bool = True
+) -> TorchTensor:
     return x.topk(dim=dim, k=k, largest=False, sorted=sorted)
