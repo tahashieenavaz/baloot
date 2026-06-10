78d46e05cff480df73f7c51e1c39e134fa9b8a9f
tahashieenavaz <tahashieenavaz@gmail.com>
2026-06-10 17:26:58 +0200

    adds dim1 and dim2 to sinkhorn

--- baloot/tensors/sinkhorn.py
+++ baloot/tensors/sinkhorn.py
@@ -3,11 +3,15 @@ from baloot.typing import TorchTensor
 
 
 def sinkhorn(
-    x: TorchTensor, iterations: int = 10, epsilon: float = 1e-6
+    x: TorchTensor,
+    iterations: int = 10,
+    epsilon: float = 1e-6,
+    dim1: int = -2,
+    dim2: int = -1,
 ) -> TorchTensor:
     torch = load_torch()
     x = torch.exp(x)
     for _ in range(iterations):
-        x = x / (x.sum(dim=1, keepdim=True) + epsilon)
-        x = x / (x.sum(dim=0, keepdim=True) + epsilon)
+        x = x / (x.sum(dim=dim1, keepdim=True) + epsilon)
+        x = x / (x.sum(dim=dim2, keepdim=True) + epsilon)
     return x
