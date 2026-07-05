791209107dd8bec6812fcf357919bb7f1014c148
tahashieenavaz <tahashieenavaz@gmail.com>
2026-07-06 00:33:18 +0200

    adds _render_template to init

--- baloot/file_management/__init__.py
+++ baloot/file_management/__init__.py
@@ -2,3 +2,4 @@ from .funnel import _save_file
 from .funnel import _load_file
 from .funnel import funnel
 from .render_template import render_template
+from .render_template import _render_template
